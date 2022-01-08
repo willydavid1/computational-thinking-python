4000bb9d8e99dcd44fa099beddf46542863c5a75
willy <willydaconceicao@habi.co>
2022-01-08 18:00:57 -0500

    Docstring

---
+++ docstring.py
@@ -0,0 +1,12 @@
+def test(param):
+    '''Description
+    param int any integer
+    returns any param + 5
+    '''
+    return param + 5
+
+def run ():
+    help(test)
+
+if __name__ == '__main__':
+    run()
